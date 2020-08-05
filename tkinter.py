40dba55644f08b176bdea9bbb24c4a4c4957f93b
katipogujayasree <69191132+katipogujayasree@users.noreply.github.com>
2020-08-05 07:49:31 +0100

    Updated the code with necessary requirements

--- tkinter.py
+++ tkinter.py
@@ -2,23 +2,23 @@ from tkinter import *
 
 root = Tk()
 
-#root.geometry("300x150") 
+#root.geometry("200x150") 
   
-w = Label(root, text ='Name of The Tool',background = 'blue', foreground ="white" , font = ("forte" ,25)).pack()
+w = Label(root, text ='Name of The Tool' , font = ("Helvetica Neue Bold",20)).pack() 
   
-frame = Frame(root) 
+frame = Frame(root)
 frame.pack()
 
 
-first_label= Label(frame, text="Please provide the below required information (All Fields marked “*” are MANDATORY)")
+first_label= Label(frame, text="Please provide the below required information (All Fields marked “ * ” are MANDATORY)")
 first_label.grid(row=0, column=0,padx = 5, pady = 15)
 
-second_label = Label(frame, text="* TICKET NUMBER",fg="red").grid(row=1, column=0,sticky='w',padx = 5, pady = 15)
+second_label = Label(frame, text="* TICKET NUMBER",fg="red",font = ("Helvetica Neue Bold",10)).grid(row=1, column=0,sticky='w',padx = 100, pady = 15)
+e1= Entry(frame).grid(row=1, column=1)
 
-e1= Entry(frame)
-e1.grid(row=1, column=0)
 
-third_label= Label(frame, text="   SEVERITY").grid(row=2, column=0, sticky='w',padx = 5, pady = 15)
+third_label= Label(frame, text="  SEVERITY",font = ("Helvetica Neue Bold",10)).grid(row=2, column=0,sticky='w',padx = 100, pady = 15)
+
 
 ButtonVar1 = IntVar()
 ButtonVar2 = IntVar()
@@ -26,46 +26,35 @@ ButtonVar3 = IntVar()
 ButtonVar4 = IntVar()
 ButtonVar5 = IntVar()
 
-e2= Radiobutton(frame, text = "1", variable = ButtonVar1)
-e2.grid(row=2, column=0,)
-e2= Radiobutton(frame, text = "2", variable = ButtonVar1)
-e2.grid(row=2, column=1)
-e2= Radiobutton(frame, text = "3", variable = ButtonVar1)
-e2.grid(row=2, column=2)
-e2= Radiobutton(frame, text = "4", variable = ButtonVar1)
-e2.grid(row=2, column=3)
-e2= Radiobutton(frame, text = "5", variable = ButtonVar1)
-e2.grid(row=2, column=4)
 
-fourth_label = Label(frame, text="* CLIENT", fg="red").grid(row=3, column=0, sticky='w',padx = 5, pady = 15)
+e2= Radiobutton(frame, text = "1", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=1,padx=40,pady=10)
+e2= Radiobutton(frame, text = "2", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=2,padx=40,pady=10)
+e2= Radiobutton(frame, text = "3", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=3,padx=40,pady=10)
+e2= Radiobutton(frame, text = "4", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=4,padx=40,pady=10)
+e2= Radiobutton(frame, text = "5", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=5,padx=40,pady=10)
 
-e3= Entry(frame)
-e3.grid(row=3, column=0)
+fourth_label = Label(frame, text="* CLIENT", fg="red",font = ("Helvetica Neue Bold",10)).grid(row=3, column=0, sticky='w',padx = 100, pady = 15)
+e3= Entry(frame).grid(row=3, column=1)
 
-fifth_label = Label(frame, text="* SITE / LOCATION", fg="red").grid(row=4, column=0, sticky='w',padx = 5, pady = 15)
 
-e4= Entry(frame)
-e4.grid(row=4, column=0)
+fifth_label = Label(frame, text="* SITE / LOCATION", fg="red",font = ("Helvetica Neue Bold",10)).grid(row=4, column=0, sticky='w',padx = 100, pady = 15)
+e4= Entry(frame).grid(row=4, column=1)
 
-sixth_label= Label(frame, text="  ISSUE TYPE").grid(row=5, column=0, sticky='w', padx = 5, pady = 15)
 
-# n = tk.StringVar() 
-# monthchoosen = ttk.Combobox(window, width = 27, textvariable = n)
+sixth_label= Label(frame, text="  ISSUE TYPE",font = ("Helvetica Neue Bold",10)).grid(row=5, column=0, sticky='w', padx = 100, pady = 15)
 
-# monthchoosen['values'] = (' January',  
-#                           ' February', 
-#                           ' March', 
-#                           ' April', 
-#                           ' May', 
-#                           ' June', 
-#                           ' July', 
-#                           ' August', 
-#                           ' September', 
-#                           ' October', 
-#                           ' November', 
-#                           ' December') 
-  
-# monthchoosen.grid(column = 1, row = 5) 
-# monthchoosen.current() 
+var1 = StringVar(frame)
+drop1 = OptionMenu(frame, var1, "A", "B", "C", "D").grid(row=5,column=1)
+
+
+Seventh_label= Label(frame, text=" CMDB",font = ("Helvetica Neue Bold",10)).grid(row=6, column=0, sticky='w',padx = 100, pady = 15)
+
+ButtonVar6 = IntVar()
+ButtonVar7 = IntVar()
+
+
+e2= Radiobutton(frame, text = "JUMPHOST SERVER", variable = ButtonVar6).grid(row=6, column=1)
+e2= Radiobutton(frame, text = "EXCEL SHEET", variable = ButtonVar7).grid(row=6, column=2)
+
 
 root.mainloop() 
