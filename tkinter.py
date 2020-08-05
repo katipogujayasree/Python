5eb5cc1219952251f1f630de6e548de97a103fef
katipogujayasree <69191132+katipogujayasree@users.noreply.github.com>
2020-08-05 16:26:37 +0100

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,60 +1,58 @@
+# importing everything that exists in tkinter module
 from tkinter import *
 
+# create window
 root = Tk()
 
-#root.geometry("200x150") 
+#Window Size
+root.geometry("550x450") #Width x Height
   
-w = Label(root, text ='Name of The Tool' , font = ("Helvetica Neue Bold",20)).pack() 
+w = Label(root, text ='Name of The Tool', font = ("Georgia",20)).pack() 
   
 frame = Frame(root)
 frame.pack()
 
-
+#Create a Label
 first_label= Label(frame, text="Please provide the below required information (All Fields marked “ * ” are MANDATORY)")
-first_label.grid(row=0, column=0,padx = 5, pady = 15)
+first_label.grid(row=0, column=0,padx = 2, pady = 15,columnspan=3)
 
-second_label = Label(frame, text="* TICKET NUMBER",fg="red",font = ("Helvetica Neue Bold",10)).grid(row=1, column=0,sticky='w',padx = 100, pady = 15)
+second_label = Label(frame, text="* TICKET NUMBER",fg="red").grid(row=1, column=0,sticky='w',padx = 50, pady = 15)
 e1= Entry(frame).grid(row=1, column=1)
 
+third_label= Label(frame, text="  SEVERITY").grid(row=2, column=0,sticky='w',padx = 50, pady = 15)
 
-third_label= Label(frame, text="  SEVERITY",font = ("Helvetica Neue Bold",10)).grid(row=2, column=0,sticky='w',padx = 100, pady = 15)
-
-
-ButtonVar1 = IntVar()
-ButtonVar2 = IntVar()
-ButtonVar3 = IntVar()
-ButtonVar4 = IntVar()
-ButtonVar5 = IntVar()
+var1 = StringVar(frame)
+drop1 = OptionMenu(frame, var1, "Sev 1", "Sev 2", "Sev 3", "Sev 4", "Sev 5").grid(row=2,column=1)
 
 
-e2= Radiobutton(frame, text = "1", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=1,padx=40,pady=10)
-e2= Radiobutton(frame, text = "2", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=2,padx=40,pady=10)
-e2= Radiobutton(frame, text = "3", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=3,padx=40,pady=10)
-e2= Radiobutton(frame, text = "4", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=4,padx=40,pady=10)
-e2= Radiobutton(frame, text = "5", variable = ButtonVar1,padx=5, pady=10).grid(row=2, column=5,padx=40,pady=10)
+fourth_label = Label(frame, text="* CLIENT", fg="red").grid(row=3, column=0, sticky='w',padx = 50, pady = 15)
 
-fourth_label = Label(frame, text="* CLIENT", fg="red",font = ("Helvetica Neue Bold",10)).grid(row=3, column=0, sticky='w',padx = 100, pady = 15)
-e3= Entry(frame).grid(row=3, column=1)
+var1 = StringVar(frame)
+drop1 = OptionMenu(frame, var1, "Client A", "Client B", "Client C", "Client D").grid(row=3,column=1)
 
 
-fifth_label = Label(frame, text="* SITE / LOCATION", fg="red",font = ("Helvetica Neue Bold",10)).grid(row=4, column=0, sticky='w',padx = 100, pady = 15)
+fifth_label = Label(frame, text="* SITE / LOCATION", fg="red").grid(row=4, column=0, sticky='w',padx = 50, pady = 15)
 e4= Entry(frame).grid(row=4, column=1)
 
 
-sixth_label= Label(frame, text="  ISSUE TYPE",font = ("Helvetica Neue Bold",10)).grid(row=5, column=0, sticky='w', padx = 100, pady = 15)
+sixth_label= Label(frame, text="  ISSUE TYPE").grid(row=5, column=0, sticky='w', padx = 50, pady = 15)
 
 var1 = StringVar(frame)
 drop1 = OptionMenu(frame, var1, "A", "B", "C", "D").grid(row=5,column=1)
 
 
-Seventh_label= Label(frame, text=" CMDB",font = ("Helvetica Neue Bold",10)).grid(row=6, column=0, sticky='w',padx = 100, pady = 15)
+Seventh_label= Label(frame, text=" CMDB").grid(row=6, column=0, sticky='w',padx = 50, pady = 15)
 
 ButtonVar6 = IntVar()
-ButtonVar7 = IntVar()
 
 
-e2= Radiobutton(frame, text = "JUMPHOST SERVER", variable = ButtonVar6).grid(row=6, column=1)
-e2= Radiobutton(frame, text = "EXCEL SHEET", variable = ButtonVar7).grid(row=6, column=2)
+e2= Radiobutton(frame, text = "JUMPHOST SERVER", variable = ButtonVar6 , value=1).grid(row=6, column=1)
+e2= Radiobutton(frame, text = "EXCEL SHEET", variable = ButtonVar6, value=2).grid(row=6, column=2)
 
+def submit():
+    return
+
+btn1 = Button(frame, text="Submit", command=submit,font = ("bauhaus 93",20), background= "blue").grid(row=7, column=2)
 
 root.mainloop() 
+
